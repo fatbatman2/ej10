d01d7353e027314ebacd07ee71f98930b4fb67c1
fatbatman2 <100149988+fatbatman2@users.noreply.github.com>
2022-05-02 09:06:11 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+while True:
+
+   basic.show_icon(IconNames.Heart)
+   basic.clear_screen()
+   basic.pause(100)
+   basic.show_icon(IconNames.SMALL_HEART)
+   basic.clear_screen()
+   basic.pause(100)
